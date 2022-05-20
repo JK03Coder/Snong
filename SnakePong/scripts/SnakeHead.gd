d7e95477642e4c334401faa613e4006197262316
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-05-20 17:23:08 +1200

    Snake Head Movement
    the snake isn't locked to the grid but still keeps that classic feel. I made it so you can't 180 from the current direction you're going just like the classic snake

--- SnakePong/scripts/SnakeHead.gd
+++ SnakePong/scripts/SnakeHead.gd
@@ -9,15 +9,14 @@ var turn_rate = 0.1
 func _physics_process(delta: float) -> void:
 	# if you press multiple keys at the same time
 	# it will prioritze the last checked keys
-	if Input.is_action_just_pressed("move_right"):
+	if Input.is_action_just_pressed("move_right") and move_dir.dot(Vector2.RIGHT) >= 0:
 		input_dir = Vector2.RIGHT
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_just_pressed("move_left") and move_dir.dot(Vector2.LEFT) >= 0:
 		input_dir = Vector2.LEFT
-	if Input.is_action_just_pressed("move_up"):
+	if Input.is_action_just_pressed("move_up") and move_dir.dot(Vector2.UP) >= 0:
 		input_dir = Vector2.UP
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_just_pressed("move_down") and move_dir.dot(Vector2.DOWN) >= 0:
 		input_dir = Vector2.DOWN
 	move_dir.x = move_toward(move_dir.x, input_dir.x, turn_rate)
 	move_dir.y = move_toward(move_dir.y, input_dir.y, turn_rate)
 	move_and_slide(move_dir * SPEED)
-
