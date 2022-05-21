40bc89e95c17a4a581802c564449b8e1b11ba4f1
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-05-21 21:25:21 +1200

    dunno what I'm doing

--- SnakePong/scripts/SnakeBody.gd
+++ SnakePong/scripts/SnakeBody.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
-const SPEED = 100
-var direction : Vector2
+var direction : Vector2 = Vector2.RIGHT
+var move_dir : Vector2 = Vector2.RIGHT
+var segment_id : int
+
 onready var parent := get_parent()
+onready var turn_rate : float = parent.turn_rate
+onready var SPEED = parent.SPEED
 
-
-func _ready():
-    set_as_toplevel(true)
+func _init():
+	set_as_toplevel(true)
 
 
 func _physics_process(delta):
-    direction = global_position.direction_to(parent.global_position).normalized()
-    move_and_slide(direction * SPEED)
+	if len(parent.turn_markers) > 0:
+		direction = global_position.direction_to(parent.turn_markers[segment_id]).normalized()
+	move_dir.x = move_toward(move_dir.x, direction.x, turn_rate)
+	move_dir.y = move_toward(move_dir.y, direction.y, turn_rate)
+	move_and_slide(move_dir * SPEED)
--- SnakePong/scripts/SnakeHead.gd
+++ SnakePong/scripts/SnakeHead.gd
@@ -1,43 +1,54 @@
 extends KinematicBody2D
 
 export(PackedScene) var body
+export(int, 0, 50) var body_segments = 20
 
 const SPEED = 100
 
 var input_dir := Vector2.RIGHT
 var move_dir := Vector2.RIGHT
 var turn_rate = 0.1
+var segment_delay : float = 0.5
+var current_segments : int = 0
+var turn_markers : Array = []
 
-onready var lag_pos := Vector2.ZERO
 onready var lag_timer := Timer.new()
+onready var body_spawn_pos : Vector2 = global_position
 
 func _ready():
 	assert(body != null, "add a scene to the export var body")
-	lag_timer.wait_time = 0.5
 	lag_timer.connect("timeout", self, "lag_timeout")
+	lag_timer.wait_time = segment_delay
 	add_child(lag_timer)
 	lag_timer.start()
 
 
 func lag_timeout():
-	var new_body = body.instance()
-	new_body.global_position = lag_pos
-	lag_pos = global_position
-	add_child(new_body)
-	lag_timer.start()
+	if current_segments <= body_segments:
+		var new_body = body.instance()
+		new_body.global_position = body_spawn_pos
+		new_body.segment_id = current_segments
+		add_child(new_body)
+		lag_timer.start()
+		current_segments += 1
 
 
-func _physics_process(delta: float) -> void:
+func update_turn(direction: Vector2):
+	input_dir = direction
+	turn_markers.append(global_position)
+
+
+func _physics_process(_delta: float) -> void:
 	# if you press multiple keys at the same time
 	# it will prioritze the last checked keys
 	if Input.is_action_just_pressed("move_right") and move_dir.dot(Vector2.RIGHT) >= 0:
-		input_dir = Vector2.RIGHT
+		update_turn(Vector2.RIGHT)
 	if Input.is_action_just_pressed("move_left") and move_dir.dot(Vector2.LEFT) >= 0:
-		input_dir = Vector2.LEFT
+		update_turn(Vector2.LEFT)
 	if Input.is_action_just_pressed("move_up") and move_dir.dot(Vector2.UP) >= 0:
-		input_dir = Vector2.UP
+		update_turn(Vector2.UP)
 	if Input.is_action_just_pressed("move_down") and move_dir.dot(Vector2.DOWN) >= 0:
-		input_dir = Vector2.DOWN
+		update_turn(Vector2.DOWN)
 	move_dir.x = move_toward(move_dir.x, input_dir.x, turn_rate)
 	move_dir.y = move_toward(move_dir.y, input_dir.y, turn_rate)
 	move_and_slide(move_dir * SPEED)
