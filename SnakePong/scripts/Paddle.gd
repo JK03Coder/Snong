5dd108230135106f9f6b932429c4c3627263d823
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-05-24 17:48:33 +1200

    snake collision hack

--- SnakePong/scripts/Paddle.gd
+++ SnakePong/scripts/Paddle.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
 export(float) var speed : float = 30.0
+export(float) var upper_limit : float = 70.0
+export(float) var lower_limit : float = 530.0
 
 var direction : Vector2
 
 
 func _physics_process(delta: float) -> void:
 
-	global_position.y = Global.ball_position.y
+	if Global.ball_position.y > upper_limit and Global.ball_position.y < lower_limit:
+		global_position.y = Global.ball_position.y
