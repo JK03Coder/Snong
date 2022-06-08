a969725e34c8fec7d52893f3e678e4f17c512531
Mage <mistfist75@gmail.com>
2022-06-08 21:24:35 +1200

    multiplayer tweaks

--- SnakePong/scripts/multiplayer.gd
+++ SnakePong/scripts/multiplayer.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
-func _on_game_over():
-		get_tree().change_scene("res://scenes/TitleScreen.tscn")
-
 
 func _on_Snake1_game_over():
 	pass # Replace with function body.
-
 
 func _on_Snake2_game_over():
 	pass # Replace with function body.
